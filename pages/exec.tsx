428ec00139fef6e1f3cd7416e14986c22aec2afc
Jaival Patel <pateljaival05@gmail.com>
2022-08-17 14:43:06 -0400

    re-factoring

---
+++ pages/exec.tsx
@@ -0,0 +1,355 @@
+
+
+
+export default function exec() {
+
+    const grid_picture = (
+        <div id="grid-picture">
+            <p>  </p>
+        </div>
+    )
+
+    const title_headers = (name: any) => {
+        return (
+            <div id="down">
+                <p><u>{name}</u></p>
+            </div>
+        )
+    }
+
+    return (
+        <div>
+        <link rel="stylesheet" href="/styles/exec.css" />
+
+    <head>
+
+        <div id="ExecutiveTitle">
+            <div id="ExecutiveTitleFont">
+                <h1> Executive Team </h1>
+            </div>
+
+            <div id="ExecutiveSubTitleFont">
+                
+                <p> Your 2023 Executive Team to Guide Your Journey in Debate </p>
+            </div>
+            
+            <div id="ViewExecButton">
+                <button id="window.location.href='#down'"> <p>View Your 2023 Execs</p></button>
+            
+                {title_headers("Presidents")}
+
+                <div id="executive-grid-container">
+                    <div id="grid-president1">
+                        <h2> Jeevan Brar </h2>
+                        <div id="grid-text">
+                            
+                            <div id="text">
+                                <p>Jeevan is leading the exceutive team to make sure that the 2022/2023 chapter is the best chapter yet. 
+                                    His love for debate has encouraged every single one of the executives and hopes to spread the same amount passion 
+                                    to TFSS this year.
+                                 </p>
+                            </div>
+
+                            {grid_picture}
+                        </div>
+                    </div>
+    
+                    <div id="grid-president2">
+                        <h2> Japnam Dhaliwal </h2>
+                        <div id="grid-text">
+                            
+                            <div id="text">
+                                <p>Japnam is leading the exceutive team to make sure that the 2022/2023 chapter is the best chapter yet. 
+                                    His love for debate and intellect reasoning has allowed him to share thought-provoking arguments in the past. He 
+                                    aims to encourage debate and the exploration of the truth throughout TFSS this year!
+                                 </p>
+                            </div>
+                            {grid_picture}
+                        </div>
+                    </div>
+
+                    
+                </div>
+                
+                {title_headers("Directors of Education")}
+
+                <div id="executive-grid-container">
+                    <div id="grid-president1">
+                        <h2> Aditya Makkar </h2>
+                        <div id="grid-text">
+                            
+                            <div id="text">
+                                <p>Aditya is in charge of creating the educational content the members will learn about Debate. His in-depth 
+                                    knowledge and past experience has allowed him to understand what is needed to become a successful debater. His goal is 
+                                    to make members enjoy the art of creating arguments and finding the right answer.
+                                 </p>
+                            </div>
+                            {grid_picture}
+                        </div>
+                    </div>
+    
+                    <div id="grid-president2">
+                        <h2> Sara Bale </h2>
+                        <div id="grid-text">
+                            
+                            <div id="text">
+                                <p>Sara is in charge of creating the educational content the members will learn about Debate. Her in-depth 
+                                    knowledge and past experience has allowed her to understand what is needed to become a successful debater. Her goal is 
+                                    to make members strong debaters not just for competitions, but for life.
+                                 </p>
+                            </div>
+                            {grid_picture}
+                        </div>
+                    </div>
+
+                </div>
+
+                {title_headers("Heads of Communications")}
+
+                <div id="executive-grid-container">
+                    <div id="grid-president1">
+                        <h2> Ashlene Metley </h2>
+
+                        <div id="grid-text">
+                            
+                            <div id="text">
+                                <p>Ashlene is in charge of the planning and executing of communication between the TFSS Debate Society and all members. 
+                                 She has been with the Debate Team with over 2 years and is excited to share her passion for debate this year for the 2022/23 Chapter!
+                                 </p>
+                            </div>
+                            {grid_picture}
+                        </div>
+
+                    </div>
+    
+                    <div id="grid-president2">
+                        <h2> Shweta Menon </h2>
+                        
+                        <div id="grid-text">
+                            
+                            <div id="text">
+                                <p>Shweta is in charge of drafting emails to chapter members, organizing events, and creating presentations for our general chapter meetings. 
+                                 She loves to debate and is determined to make the 2022/2023 chapter the best one yet!
+                                 </p>
+                            </div>
+                            {grid_picture}
+                        </div>
+
+                    </div>
+
+                </div>
+
+                {title_headers("Heads of Promotions")}
+
+                <div id="executive-grid-container">
+                    <div id="grid-president1">
+                        <h2> Pious Hunjan </h2>
+                        <div id="grid-text">
+                            
+                            <div id="text">
+                                <p>Pious loves to debate and her goal is to spread the interest of debate throughout TFSS.
+                                    She will be creating social media posts, interacting with potential chapter members, finding other creative outputs to promote our chapter, and announcing chapter news.
+                                 </p>
+                            </div>
+                            {grid_picture}
+                        </div>
+                    </div>
+    
+                    <div id="grid-president2">
+                        <h2> Dilnazz Matharu </h2>
+                        <div id="grid-text">
+                            
+                            <div id="text">
+                                <p>Dilnazz loves to debate and her goal is to spread the interest of debate throughout TFSS.
+                                    She will be creating social media posts, interacting with potential chapter members, finding other creative outputs to promote our chapter, and announcing chapter news.
+                                 </p>
+                            </div>
+                            {grid_picture}
+                        </div>
+                    </div>
+
+                    <div id="grid-president3">
+                        <h2> Tanisha Gottemukula </h2>
+                        <div id="grid-text">
+                            
+                            <div id="text">
+                                <p>Tanisha loves to debate and her goal is to spread the interest of debate throughout TFSS.
+                                    She will be creating social media posts, interacting with potential chapter members, finding other creative outputs to promote our chapter, and announcing chapter news.
+                                 </p>
+                            </div>
+                            {grid_picture}
+                        </div>
+                    </div>
+
+                </div>
+
+                {title_headers("Education Team")}
+
+                <div id="executive-grid-container">
+                    <div id="grid-president1">
+                        <h2> 
+
+                            Anhad Kaur </h2>
+                            <div id="grid-text">
+                            
+                                <div id="text">
+                                    <p>Anhad is experienced with debating and her passion for the discipline has allowed her to teach
+                                        teaching weekly debate lessons to a class of students alongside other education team members and working with Directors of Education on educational planning. 
+
+                                        
+                                     </p>
+                                </div>
+                                <div id="grid-picture">
+                                    <p> </p>
+                                </div>
+                            </div>
+                    </div>
+                    
+    
+                    <div id="grid-president2">
+                        <h2> 
+
+                            Mansi Arora </h2>
+                            <div id="grid-text">
+                            
+                                <div id="text">
+                                    <p>Mansi is experienced with debating and her passion for the discipline has allowed her to teach
+                                        teaching weekly debate lessons to a class of students alongside other education team members and working with Directors of Education on educational planning. 
+
+                                        
+                                     </p>
+                                </div>
+                                {grid_picture}
+                            </div>
+                    </div>
+
+                    <div id="grid-president2">
+                        <h2> 
+                            Saachi Kohli </h2>
+                        
+                            <div id="grid-text">
+                            
+                                <div id="text">
+                                    <p>Saachi is experienced with debating and her passion for the discipline has allowed her to teach
+                                        teaching weekly debate lessons to a class of students alongside other education team members and working with Directors of Education on educational planning. 
+
+                                        
+                                     </p>
+                                </div>
+                                {grid_picture}
+                            </div>
+                    </div>
+
+                    <div id="grid-president2">
+                        <h2> 
+                            Elena Campos Perez </h2>
+                            <div id="grid-text">
+                            
+                                <div id="text">
+                                    <p>Elena is experienced with debating and her passion for the discipline has allowed her to teach
+                                        teaching weekly debate lessons to a class of students alongside other education team members and working with Directors of Education on educational planning. 
+
+                                        
+                                     </p>
+                                </div>
+                                {grid_picture}
+                            </div>
+                    </div>
+                    
+                    <div id="grid-president2">
+                        <h2> Ester Campos Perez </h2>
+                        <div id="grid-text">
+                            
+                            <div id="text">
+                                <p>Ester is experienced with debating and her passion for the discipline has allowed her to teach
+                                    teaching weekly debate lessons to a class of students alongside other education team members and working with Directors of Education on educational planning. 
+
+                                    
+                                 </p>
+                            </div>
+                            {grid_picture}
+                        </div>
+                    </div>
+
+                    <div id="grid-president2">
+                        <h2> 
+                            Maanya Dahiya </h2>
+                            <div id="grid-text">
+                            
+                                <div id="text">
+                                    <p>Maanya is experienced with debating and her passion for the discipline has allowed her to teach
+                                        teaching weekly debate lessons to a class of students alongside other education team members and working with Directors of Education on educational planning. 
+
+                                        
+                                     </p>
+                                </div>
+                                {grid_picture}
+                            </div>
+                            
+                    </div>
+
+                    <div id="grid-president3">
+                        <h2> Amreen Sandhu </h2>
+                        <div id="grid-text">
+                            
+                            <div id="text">
+                                <p>Amreen is experienced with debating and her passion for the discipline has allowed her to teach
+                                    teaching weekly debate lessons to a class of students alongside other education team members and working with Directors of Education on educational planning. 
+
+                                    
+                                 </p>
+                            </div>
+                            {grid_picture}
+                        </div>
+                    </div>
+
+                </div>
+
+                {title_headers("Web & Tech")}
+
+                <div id="executive-grid-container">
+                    <div id="grid-president1">
+                        <h2> Jaival Patel </h2>
+
+                        <div id="grid-text">
+                            
+                            <div id="text">
+                                <p>Jaival is in charge of the "tech-based stuff" of Debate. He hopes that his passion for technology allows 
+                                    members to make the most out of the Debate Club. From getting recent information at their fingertips to lessons,
+                                    Jaival will ensure that the tools are available for the members to explore the art of debate.
+                                 </p>
+                            </div>
+                            {grid_picture}
+                        </div>
+                    </div>
+    
+                    <div id="grid-president2">
+                        <h2> Rajan Aggarwal </h2>
+                        
+                            <div id="grid-text">
+                            
+                                <div id="text">
+                                    <p> Rajan Aggarwal is in charge with the creation, maintenance, and upkeep of all TFSS Debate online work (websites, large virtual meetings, google classrooms.) 
+                                        His goal is to make sure that communications between the members and the club are smooth and accessible.
+                                     </p>
+                                </div>
+                                {grid_picture}
+                            </div>
+
+                        
+                    </div>
+
+
+                </div>
+
+                
+
+
+            </div>
+
+        </div>
+
+    </head>
+    </div>
+  );
+}
